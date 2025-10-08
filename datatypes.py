2c158a156d231a21ca11c48f465381d984bd374e
titussony <tituphlip2018@gmail.com>
2025-10-08 15:25:20 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+
+int_var = 10
+print( type(int_var))
+
+
+float_var = 10.5
+print( type(float_var))
+
+
+str_var = "Hello, Python!"
+print(type(str_var))
+
+
+list_var = [1, 2, 3, "Python", 4.5]
+print( type(list_var))
+
+
+set_var = {1, 2, 3, 4, 5}
+print( type(set_var))
+
+
+
+
