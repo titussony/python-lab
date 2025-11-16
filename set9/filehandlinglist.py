55081770674bb16e9846e936fa822defd0c13c63
Titus sony <tituphlip2018@gmail.com>
2025-11-16 11:05:06 +0530

    Add files via upload

---
+++ set9/filehandlinglist.py
@@ -0,0 +1,19 @@
+file = open("file2.txt", "w")
+file.write("hello world!\nwelcome to python programing\nThank you")
+file.close()
+list = []
+file = open("file2.txt", "r")
+list = [line.strip() for line in file]
+file.close()
+print(list)
+
+
+
+
+
+
+
+
+
+
+
